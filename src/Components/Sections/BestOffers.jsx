1259b0e5ef0ae4e3be57e8ef9153933a24c6797a
Md. Aminul Islam Shaon <aminul007.ais@gmail.com>
2024-04-14 02:33:48 +0600

    finilizing the latest offer section

--- src/Components/Sections/BestOffers.jsx
+++ src/Components/Sections/BestOffers.jsx
@@ -1,4 +1,5 @@
 import { useLoaderData } from 'react-router-dom'
+import './style-section.css'
 const BestOffers = () => {
 
 
@@ -12,30 +13,31 @@ const BestOffers = () => {
         'col-span-2'
     ]
     return (
-        <div className='w-[90%] p-6' >
-            <h1>Best offers this week</h1>
-            <p>Unlock your dream home with our exclusive offer this week! Enjoy unbeatable discounts on property prices, making homeownership more affordable than ever.</p>
-            <div className='grid grid-cols-4 gap-2 border-2 border-green-700 h-[600px] '>
+        <div className='mt-20'>
+            <div className=' text-center'>
+                <h1 className='text-4xl font-bold mb-5'>Best offers this week</h1>
+                <p className='w-[600px] mx-auto text-[#231F20B3]'>Unlock your dream home with our exclusive offer this week! Enjoy unbeatable discounts on property prices, making homeownership more affordable than ever.</p>
+            </div>
+            <div className='grid grid-cols-4 gap-2 h-[600px]'>
                 {
                     datas.slice(0, 5).map((data, index) =>
                         <div key={index}
-                            className={`${imageClasses[index]} relative overflow-hidden `}>
-                            <div className='w-full h-full absolute right-2 flex flex-col outline-dashed  space-y-2 z-10'>
-                                <h1 className='shadow-2xl text-2xl text-white font-bold '>{data.location}</h1>
-                                <p className='text-white font-bold text-[18px]'>{data.price}$</p>
+                            className={`${imageClasses[index]} relative overflow-hidden`}>
+                            <div className=' absolute z-20 bottom-2 right-3 '>
+                                <p className='font-bold text-[18px] bg-white text-black px-2 rounded-sm'>${data.price}</p>
                             </div>
-                            <div className='w-full h-full absolute flex flex-col items-startz-10'>
-                                <h3 className='text-lg font-bold '>{data.simple_address}</h3>
-                                <h3 className='text-[16px] font-semibold '>{data.full_address}</h3>
+                            <div className='absolute bottom-2 left-1 z-20'>
+                                <h3 className='text-lg font-bold text-white'>{data.simple_address}</h3>
+                                <h3 className='text-[13px] font-semibold text-white'>{data.full_address}</h3>
                             </div>
                             <div className='absolute w-full h-full z-0'>
                                 <img
                                     key={index}
                                     src={data.img}
-                                    className='w-full h-full object-cover z-10' />
+                                    className='w-full h-full object-cover zoom-effect' />
                             </div>
-                            <div className=' absolute overflow-clip p-5 z-20 border-2 border-red-600 w-full h-full flex justify-end bottom-6'>
-                                <button className="btn bg-gradient-to-t from-[#8E793E] to-[#AD974F] border-0 hover:bg-gradient-to-b hover:from-[#6b5d41] hover:to-[#ac9b65] text-white">View Details</button>
+                            <div className=' absolute top-2 left-2 z-20'>
+                                <p className='bg-[#00000066] text-white font-bold px-2 py-1 rounded-sm'>For {data.Status}</p>
                             </div>
                         </div>
                     )
